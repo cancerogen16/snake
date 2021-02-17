758549f869aa3f1d2e0b6096fd302cf5df04ec36
cancerogen-kraken <don544@mail.ru>
2021-02-17 11:36:11 +0500

    HorizontalLine

--- snake/Program.cs
+++ snake/Program.cs
@@ -13,24 +13,8 @@ namespace snake
             Point p2 = new Point(4, 5, '#');
             p2.Draw();
 
-            List<string> sList = new List<string>();
-            sList.Add("*");
-            sList.Add("#");
-            sList.Add("+");
-
-            foreach(string s in sList)
-            {
-                Console.WriteLine(s);
-            }
-
-            List<Point> pList = new List<Point>();
-            pList.Add(p1);
-            pList.Add(p2);
-
-            foreach (Point p in pList)
-            {
-                Console.WriteLine(p);
-            }
+            HorizontalLine line = new HorizontalLine(4, 10, 5, '+');
+            line.Draw();
 
             Console.ReadLine();
         }
