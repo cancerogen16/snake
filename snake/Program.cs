3db6a0ef836e0a28629274fb28abce171a74303f
cancerogen-kraken <don544@mail.ru>
2021-02-17 11:16:26 +0500

    List<Point> pList = new List<Point>();

--- snake/Program.cs
+++ snake/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace snake
 {
@@ -11,6 +12,25 @@ namespace snake
 
             Point p2 = new Point(4, 5, '#');
             p2.Draw();
+
+            List<string> sList = new List<string>();
+            sList.Add("*");
+            sList.Add("#");
+            sList.Add("+");
+
+            foreach(string s in sList)
+            {
+                Console.WriteLine(s);
+            }
+
+            List<Point> pList = new List<Point>();
+            pList.Add(p1);
+            pList.Add(p2);
+
+            foreach (Point p in pList)
+            {
+                Console.WriteLine(p);
+            }
 
             Console.ReadLine();
         }
